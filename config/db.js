5f55d41b597d6e63bab17f951dfb419c62df8f3a
dasosjt <diegososajt@gmail.com>
2020-03-03 06:40:33 -0600

    Update db credentials

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 module.exports = {
-  user: 'postgres',
-  database: 'classroom',
-  password: '',
-  host: 'localhost',
+  user: 'crazapqigfykuu',
+  database: 'd3rorna6kf8ode',
+  password: '8e58260b2469fe04622d2c28f06d49d6fd01b48d3e10249f83a272aa17a19035',
+  host: 'ec2-107-20-186-238.compute-1.amazonaws.com',
   port: 5432,
   max: 100,
   idleTimeoutMillis: 20000,
